215895115fd9964031777d58d81fd69c574a462a
Shalabh Vyas <shalabhvyas@gmail.com>
2021-02-14 08:42:38 -0500

    importing types from slate

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import "./App.css";
 
 import { Editable, Slate, withReact } from "slate-react";
-import React, { useEffect, useMemo, useState } from "react";
-
+import React, { useMemo, useState } from "react";
+import type { Node } from "slate";
 import { createEditor } from "slate";
 
 function App() {
   const editor = useMemo(() => withReact(createEditor()), []);
-  const [value, setValue] = useState<Descendant[]>([
+  const [value, setValue] = useState<Node[]>([
     {
       type: "paragraph",
       children: [{ text: "A line of text in a paragraph." }],
