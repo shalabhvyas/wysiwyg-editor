084eb0b9753bdb4dbdf8269798a0bbcf23793fe5
Shalabh Vyas <shalabhvyas@gmail.com>
2021-02-16 18:24:07 -0500

    wip on getting links to work

--- src/components/LinkEditor.react.js
+++ src/components/LinkEditor.react.js
@@ -1,16 +1,56 @@
 import "./LinkEditor.css";
 
-// import { useCallback, useContext, useState } from "react";
+import { Editor, Transforms } from "slate";
+import { useCallback, useState } from "react";
 
-// import { EditorAPIContext } from "./Editor.react";
+import isUrl from "is-url";
+import { useEditor } from "slate-react";
 
-export default function LinkEditor({ node, path }) {
-  //   const [linkText, setLinkText] = useState(text);
-  //   const onLinkTextChange = useCallback(
-  //     (event) => setLinkText(event.targer.value),
-  //     [setLinkText]
-  //   );
+export default function LinkEditor({ attributes, element, children }) {
+  const editor = useEditor();
+  const [linkText, setLinkText] = useState(element.linkText);
+  const onLinkTextChange = useCallback(
+    (event) => {
+      setLinkText(event.target.value);
+    },
+    [setLinkText]
+  );
+
+  const [linkURL, setLinkURL] = useState(element.url);
+  const onLinkURLChange = useCallback(
+    (event) => setLinkURL(event.target.value),
+    [setLinkURL]
+  );
+
+  const onApply = useCallback(() => {
+    const [_, path] = Editor.above(editor, {
+      match: (n) => n.type === "link",
+    });
+    // update URL
+    Transforms.setNodes(editor, { url: linkURL }, { at: path });
+    // remove text
+  }, [editor, linkText, linkURL]);
   // get text for the node at path and show that here for editing.
   //   const editorInstance = useContext(EditorAPIContext);
-  return <span className="link-editor">Path:{path}</span>;
+  return (
+    <span className={"link-editor"} contentEditable={false} {...attributes}>
+      <input
+        style={{ margin: 8 }}
+        type="text"
+        value={linkText}
+        onChange={onLinkTextChange}
+      />
+      <input
+        style={{ margin: 8 }}
+        type="text"
+        value={linkURL}
+        onChange={onLinkURLChange}
+      />
+      {/* Check if URL is valid */}
+      <button disabled={!isUrl(linkURL)} onClick={onApply}>
+        Apply
+      </button>
+      {children}
+    </span>
+  );
 }
