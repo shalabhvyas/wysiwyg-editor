4b4ce6520ad9477db8a5cb3b38b271dba08a95ce
Shalabh Vyas <shalabhvyas@gmail.com>
2021-02-18 00:42:11 -0500

    images done for now

--- src/components/Image.react.js
+++ src/components/Image.react.js
@@ -2,12 +2,16 @@ import "./Image.css";
 
 import { Editor, Transforms } from "slate";
 import React, { useCallback, useState } from "react";
+import { useEditor, useFocused, useSelected } from "slate-react";
 
-import { useEditor } from "slate-react";
+import classNames from "classnames";
 
 const Image = ({ attributes, children, element }) => {
   const [isEditingCaption, setEditingCaption] = useState(false);
   const editor = useEditor();
+
+  const selected = useSelected();
+  const focused = useFocused();
 
   const onCaptionChange = useCallback(
     (event) => {
@@ -32,34 +36,45 @@ const Image = ({ attributes, children, element }) => {
   );
 
   return (
-    <div {...attributes} contentEditable={false} className="image-container">
-      {!element.isUploading && element.url != null ? (
-        <img
-          src={String(element.url)}
-          alt={element.caption}
-          className={"image"}
-        />
-      ) : (
-        <div className={"image-upload-placeholder"}>Placeholder</div>
-      )}
-      {isEditingCaption ? (
-        <textarea
-          type="text"
-          value={element.caption}
-          className={"image-caption-input"}
-          onChange={onCaptionChange}
-          onBlur={onToggleCaptionEditMode}
-        />
-      ) : (
-        <div
-          className={"image-caption-read-mode"}
-          onClick={onToggleCaptionEditMode}
-        >
-          {element.caption}
-        </div>
-      )}
+    <>
+      <div
+        {...attributes}
+        contentEditable={false}
+        className={classNames({
+          "image-container": true,
+          "is-selected": selected && focused,
+        })}
+      >
+        {!element.isUploading && element.url != null ? (
+          <img
+            src={String(element.url)}
+            alt={element.caption}
+            className={"image"}
+          />
+        ) : (
+          <div className={"image-upload-placeholder"}>Placeholder</div>
+        )}
+        {isEditingCaption ? (
+          <textarea
+            type="text"
+            value={element.caption}
+            className={"image-caption-input"}
+            onChange={onCaptionChange}
+            onBlur={onToggleCaptionEditMode}
+          />
+        ) : (
+          <div
+            className={"image-caption-read-mode"}
+            onClick={onToggleCaptionEditMode}
+          >
+            {element.caption}
+          </div>
+        )}
+      </div>
+      {/* Talk about why void elements still need to render children and why children need to be rendered outside
+      content editable. */}
       {children}
-    </div>
+    </>
   );
 };
 
