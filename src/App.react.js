ceb226617c1503e1126cf716bcdb4e9b4bb8ee79
Shalabh Vyas <shalabhvyas@gmail.com>
2021-02-19 15:18:54 -0500

    Updated document contents

--- src/App.react.js
+++ src/App.react.js
@@ -10,41 +10,61 @@ function App() {
   const [document, updateDocument] = useState([
     {
       type: "h1",
-      children: [{ text: "Heading H1" }],
+      children: [{ text: "Document Title (Heading H1)" }],
     },
     {
       type: "h2",
-      children: [{ text: "Heading H2" }],
+      children: [{ text: "Subtitle (Heading H2)" }],
     },
     {
       type: "paragraph",
       children: [
-        { text: "A line of text in a paragraph." },
-        { text: " Rich", bold: true },
-        { text: " text, " },
+        {
+          text:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        },
+        { text: "Duis aute irure dolor", bold: true },
+        {
+          text:
+            " in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        },
         {
           type: "link",
           url: "https://www.google.com",
           children: [
-            { text: "Text inside link." },
-            { text: "Bold text inside link.", bold: true },
+            { text: "Blandit aliquam etiam erat velit scelerisque in dictum." },
+            {
+              text:
+                "Ac felis donec et odio pellentesque diam volutpat commodo.",
+              bold: true,
+            },
           ],
         },
-        { text: "much", italic: true },
-        { text: " better than a " },
-        { text: "<textarea>", code: true },
+        {
+          text: "Bibendum est ultricies integer quis auctor elit sed.",
+          italic: true,
+        },
+        { text: "fooVariable", code: true },
+        { text: "Lectus mauris ultrices eros in cursus" },
+        { text: " turpis", underline: true },
+        { text: " massa." },
       ],
     },
     {
       id: uuid(),
       type: "image",
-      url: "/photos/biwi.jpg",
-      caption: "Beauty",
+      url: "/photos/puppy.jpg",
+      caption: "Puppy",
       children: [{ text: "" }],
     },
     {
       type: "paragraph",
-      children: [{ text: "dfjlsdfsdf" }],
+      children: [
+        {
+          text:
+            "Cras maximus auctor congue. Sed ultrices elit quis tortor ornare, non gravida turpis feugiat. Morbi facilisis sodales sem quis feugiat. Vestibulum non urna lobortis, semper metus in, condimentum ex. Quisque est justo, egestas sit amet sem ac, auctor ultricies lacus. Pellentesque lorem justo, rhoncus ut magna sit amet, rhoncus posuere libero.",
+        },
+      ],
     },
   ]);
 
