6ab920ed10014060c5b912e449786b2fce35d5db
Shalabh Vyas <shalabhvyas@gmail.com>
2021-03-18 16:06:37 -0400

    Updating styles on comment popover.

--- src/components/CommentThreadPopover.js
+++ src/components/CommentThreadPopover.js
@@ -42,34 +42,46 @@ export default function CommentThreadPopover({ editorOffsets, textNode }) {
   return (
     <NodePopover
       editorOffsets={editorOffsets}
+      isBodyFullWidth={true}
       node={textNode}
       className={"comment-thread-popover"}
     >
-      {comments.map(({ text, author }, index) => (
-        <div key={index}>
-          <div>{text}</div>
-          <div>{author}</div>
-        </div>
-      ))}
-      <div>
-        <InputGroup>
-          <Form.Control
-            autoFocus={true}
-            type="text"
-            value={commentText}
-            onChange={onCommentTextChange}
-          />
-          <InputGroup.Append>
-            <Button
-              variant="outline-primary"
-              active={commentText.length > 0}
-              onClick={onClick}
-            >
-              <i className={`bi bi-arrow-right-circle-fill`} />
-            </Button>
-          </InputGroup.Append>
-        </InputGroup>
+      <div className={"comment-list"}>
+        {comments.map((comment, index) => (
+          <CommentRow key={index} comment={comment} />
+        ))}
+      </div>
+      <div className={"comment-input-wrapper"}>
+        <Form.Control
+          autoFocus={true}
+          bsPrefix={"comment-input form-control"}
+          placeholder={"Type a comment"}
+          type="text"
+          value={commentText}
+          onChange={onCommentTextChange}
+        />
+        <Button
+          variant="primary"
+          disabled={commentText.length === 0}
+          onClick={onClick}
+        >
+          Comment
+        </Button>
       </div>
     </NodePopover>
+  );
+}
+
+function CommentRow({ comment: { author, text, creationTime } }) {
+  return (
+    <div className={"comment-row"}>
+      <div className="comment-author-photo">
+        <i className="bi bi-person-circle comment-author-photo"></i>
+      </div>
+      <div>
+        <span className="comment-author-name">{author}</span>
+        <div className="comment-text">{text}</div>
+      </div>
+    </div>
   );
 }
